808187c406400ae44be39fe95f0c11aaabfc846d
Anand Doshi <anand@erpnext.com>
2013-09-29 13:20:28 +0530

    converted functions into class methods, chart settings editor in modal

--- js/chart-builder.js
+++ js/chart-builder.js
@@ -2,180 +2,283 @@
 // 1. Type selection based on first 10 rows
 // 1. Option to select Header Row
 // 1. Transposer - Slick Grid should change
-// 1. Dialog Box to select options
-// 1. Pie chart
-// 1. Get settings
 // 1. Save settings
-// 1. Checked Rows - Save and Retrieve
 
 $(document).ready(function() {
-	get_csv();
+	window.chart_builder = new ChartBuilder({
+		url: "wb-india.csv",
+		title: "World Bank - India"
+	});
+	
+	$(window).on("resize", function() {
+		window.chart_builder.set_chart_width();
+		window.chart_builder.render_chart();
+	});
 });
 
-window.grid;
-
-var get_csv = function() {
-	$.get("wb-india.csv", function(data) {
-		data = CSVToArray(data);
-		show_in_grid(data);
-		set_column_selects();
-		$(window).trigger("resize");
-		show_chart();
-	});
-};
-
-var show_in_grid = function(data) {
-	var columns = [];
-	var checkboxSelector = new Slick.CheckboxSelectColumn({
-	      cssClass: "slick-cell-checkboxsel"
-	    });
-    columns.push(checkboxSelector.getColumnDefinition());
+var ChartBuilder = Class.extend({
+	init: function(opts) {
+		// opts = {
+		// 	url: "wb-india.csv"
+		// 	
+		// }
+		$.extend(this, opts);
+		this.set_title(this.title);
+		this.make_conf_editor();
+		this.get_csv(this.url);
+	},
 	
-	for(var i=0, j=data[0].length; i < j; i++) {
-		var d = data[0][i];
-		var v = d.toLowerCase().replace(/ /g, "_");
-		columns.push({
-			id: v, name: d, field: v
-		})
-	}
+	set_title: function(title) {
+		if(!title) title = "Chart Builder";
+		$("head title").text(title);
+		$(".navbar-brand").text(title);
+	},
 	
-	var objlist = [];
-	for(var ri=1, rlen=data.length; ri < rlen; ri++) {
-		var row = {};
-		for(var ci=0, clen=data[ri].length; ci < clen; ci++) {
-			row[columns[ci+1].field] = data[ri][ci];
-			
-			// better type selection
-			if(!columns[ci+1].type && data[ri][ci] && !isNaN(data[ri][ci])) columns[ci+1].type = "number";
-		}
-		row["color"] = random_color().join(",");
-		objlist.push(row);
-	}
-
-	var options = {
-	  enableCellNavigation: true,
-	  enableColumnReorder: false
-	};
-	
-    window.grid = new Slick.Grid("#slickgrid", objlist, columns, options);
-	grid.setSelectionModel(new Slick.RowSelectionModel({selectActiveRow: false}));
-	grid.registerPlugin(checkboxSelector);
-	grid.setSelectedRows([0, 1, 2]);
-	grid.getSelectionModel().onSelectedRangesChanged.subscribe(function() {
-		window.show_chart();
-	});
-}
-
-var random_color = function() {
-	var r = Math.floor(Math.random() * 256),
-        g = Math.floor(Math.random() * 256),
-        b = Math.floor(Math.random() * 256);
-	return [r, g, b];
-}
-
-var set_column_selects = function() {
-	window.start_column = $("#start-column").on("change", show_chart);
-	window.end_column = $("#end-column").on("change", show_chart);
-	window.legend_column = $("#legend-column").on("change", show_legend);
-	var columns = grid.getColumns();
-	var len = columns.length;
-	var start_val, end_val;
-	$.each(columns, function(i, v) {
-		window.start_column.append('<option value="'+ i +'">'+v.name+'</option>');
-		if(v.type==="number") {
-			end_val = i;
-			start_val = (i - 10) >= 0 ? (i - 10) : 0;
-		} else {
-			window.legend_column.append('<option value="'+ i +'">'+v.name+'</option>');
-		}
-	});
-	window.end_column.html(window.start_column.html());
-	window.start_column.val(start_val);
-	window.end_column.val(end_val);
-}
-
-var show_chart = function() {
-	if(!grid) return;
-	var columns = $.map(grid.getColumns(), function(v, i) { 
-		return (i >= window.start_column.val() && i<=window.end_column.val()) ? v.name : null;
-	});
-	var data = grid.getData();
-	var datasets = [];
-
-	$.each(grid.getSelectedRows(), function(i, rowid) {
-		var row = [];
-		for(var i=0, l=columns.length; i<l; i++) {
-			row.push(parseFloat(data[rowid][columns[i]] || 0));
-		}
-		color = data[rowid]["color"];
-		datasets.push({
-			fillColor : "rgba("+color+",0.5)",
-			strokeColor : "rgba("+color+",1)",
-			pointColor : "rgba("+color+",1)",
-			pointStrokeColor : "#fff",
-			data : row
-		});
-	});
-	
-	
-	var opts = {
-		labels : columns,
-		datasets : datasets
-	};
-	
-	var ctx = document.getElementById("chart").getContext("2d");
-	var myNewChart = new Chart(ctx).Line(opts);
-	
-	show_legend();
-	
-	var canvas_height = $("#chart").parent().height();
-	var window_height = $(window).height() - 50;
-	if(window_height > canvas_height) {
-		canvas_height = window_height;
-	}
-	$("#slickgrid").css("height", canvas_height).attr("height", canvas_height);
-	grid.resizeCanvas();
-}
-
-var show_legend = function() {
-	var data = grid.getData();
-	var colid = $("#legend-column").val();
-	var fieldname = grid.getColumns()[colid].field;
-	var $legend = $("#legend").empty();
-	$.each(grid.getSelectedRows(), function(i, rowid) {
-		var row = data[rowid];
-		$legend.append('<div class="row">\
-				<div class="legend-circle" style="background-color: rgba('+row.color+',0.5); \
-					border: 2px solid rgb('+row.color+')"></div>\
-				<span>'+row[fieldname]+'</span>\
-			</div>');
-	});
-}
-
-$(window).on("resize", function() {
-	var $canvas = $("#chart");
-	$canvas.attr("width", $canvas.parent().width());
-	show_chart();
-});
-
-var get_modal = function(content) {
-	var modal = $('<div class="modal" style="overflow: auto;" tabindex="-1">\
-		<div class="modal-dialog">\
-			<div class="modal-content">\
-				<div class="modal-header">\
-					<a type="button" class="close"\
-						data-dismiss="modal" aria-hidden="true">&times;</a>\
-					<h4 class="modal-title">Edit HTML</h4>\
-				</div>\
-				<div class="modal-body ui-front">\
-					<textarea class="form-control" \
-						style="min-height: 200px; margin-bottom: 10px;\
-						font-family: Monaco, Fixed">'+content+'</textarea>\
-					<button class="btn btn-success">Update</button>\
+	make_conf_editor: function() {
+		var me = this;
+		this.conf_editor = $('<div class="modal" style="overflow: auto;" tabindex="-1">\
+			<div class="modal-dialog">\
+				<div class="modal-content">\
+					<div class="modal-header">\
+						<a type="button" class="close"\
+							data-dismiss="modal" aria-hidden="true">&times;</a>\
+						<h4 class="modal-title">Edit Settings</h4>\
+					</div>\
+					<div class="modal-body ui-front">\
+					<div class="row">\
+					    <div class="col-md-6" style="">\
+					        <div class="form-group" style="position: static;">\
+					            <label for="chart-type">Chart Type</label>\
+					            <select class="form-control" id="chart-type">\
+									<option value="Line">Line</option>\
+								    <option value="Bar">Bar</option>\
+								    <option value="Radar">Radar</option>\
+								</select>\
+					            <p class="help-block">One of Line, Bar, Radar</p>\
+					        </div>\
+					        <div class="form-group">\
+					            <label for="start-column">First Column</label>\
+					            <select class="form-control" id="start-column"></select>\
+					            <p class="help-block">First column to plot data</p>\
+					        </div>\
+					        <div class="form-group" style="padding-right: 20px; position: static;">\
+					            <button type="button" class="btn btn-default close-conf-editor">Close</button>\
+					        </div>\
+					    </div>\
+					    <div class="col-md-6" style="">\
+					        <div class="form-group">\
+					            <label for="legend-column">Legend</label>\
+					            <select class="form-control" id="legend-column"></select>\
+					            <p class="help-block">Column which describes the data</p>\
+					        </div>\
+					        <div class="form-group">\
+					            <label for="end-column">Last Column</label>\
+					            <select class="form-control" id="end-column"></select>\
+					            <p class="help-block">Last column to plot data</p>\
+					        </div>\
+					    </div>\
+					</div>\
+					</div>\
 				</div>\
 			</div>\
-		</div>\
-		</div>').appendTo(document.body);
+			</div>')
+			.appendTo(window.document.body);
+			
+		$("#chart-builder-edit").on("click", function() {
+			me.conf_editor.show();
+		});
 		
-	return modal;
-};
+		// TODO fix this bug
+		this.conf_editor.find(".close, .close-conf-editor").on("click", function() {
+			me.conf_editor.hide();
+		});
+		
+		this.chart_select = this.conf_editor.find("#chart-type")
+			.on("change", function() { me.render_chart(); });
+		this.start_select = this.conf_editor.find("#start-column")
+			.on("change", function() { me.render_chart(); });
+		this.end_select = this.conf_editor.find("#end-column")
+			.on("change", function() { me.render_chart(); });
+		this.legend_select = this.conf_editor.find("#legend-column")
+			.on("change", function() { me.render_legend(); });
+	},
+	
+	get_csv: function(url) {
+		var me = this;
+		$.get(url, function(data) {
+			me.data = CSVToArray(data);
+			me.set_conf();
+			me.render_grid();
+			me.set_column_selects();
+			me.set_chart_width();
+			me.render_chart();
+		});
+	},
+	
+	set_conf: function(conf) {
+		if(!conf) {
+			conf = {
+				head_rowid: 0,
+				selected_rowids: [0, 1, 2],
+				start_colid: 0,
+				end_colid: 0,
+				legend_colid: 0,
+				chart_type: "Line"
+			};
+		}
+		this.conf = conf;
+		if(this.conf.chart_type) this.chart_select.val(this.conf.chart_type)
+	},
+	
+	get_conf: function() {
+		return this.conf;
+	},
+	
+	render_grid: function(data) {
+		var me = this;
+		if(!data) data = this.data;
+		this.set_columns();
+		this.set_objlist();
+		
+		var options = {
+			enableCellNavigation: true,
+			enableColumnReorder: false
+		};
+		
+		this.grid = new Slick.Grid("#slickgrid", this.objlist, this.columns, options);
+		this.grid.setSelectionModel(new Slick.RowSelectionModel({selectActiveRow: false}));
+		this.grid.registerPlugin(this.checkboxSelector);
+		this.grid.setSelectedRows(this.conf.selected_rowids);
+		this.grid.getSelectionModel().onSelectedRangesChanged.subscribe(function() {
+			me.render_chart();
+		});
+		
+		this.grid_data = this.grid.getData();
+	},
+	
+	set_columns: function() {
+		this.columns = [];
+		
+		this.checkboxSelector = new Slick.CheckboxSelectColumn({
+	      cssClass: "slick-cell-checkboxsel"
+	    });
+		this.columns.push(this.checkboxSelector.getColumnDefinition());
+		
+		var head_row = this.data[this.conf.head_rowid];
+		for(var i=0, len=head_row.length; i < len; i++) {
+			var name = head_row[i];
+			var id = name.toLowerCase().replace(/ /g, "_");
+			this.columns.push({id: id, name: name, field: id});
+		}
+	},
+	
+	set_objlist: function() {
+		this.objlist = [];
+		for(var ri=(this.conf.head_rowid + 1), rlen=this.data.length; ri<rlen; ri++) {
+			var row = {rgb: this.random_rgb().join(",")};
+			for(var ci=1, clen=this.columns.length; ci < clen; ci++) {
+				var val = this.data[ri][ci-1];
+				row[this.columns[ci].field] = val;
+				
+				// TODO better type identifications
+				if(val && !isNaN(val)) this.columns[ci].type = "number";
+			}
+			this.objlist.push(row);
+		}
+	},
+	
+	random_rgb: function() {
+		var r = Math.floor(Math.random() * 256),
+	        g = Math.floor(Math.random() * 256),
+	        b = Math.floor(Math.random() * 256);
+		return [r, g, b];
+	},
+	
+	set_column_selects: function() {
+		var me = this;
+		var start_colid, end_colid;
+		for(var i=1, l=this.columns.length; i<l; i++) {
+			var name = this.columns[i].name;
+			this.start_select.append('<option value="'+i+'">'+name+'</option>');
+			if(this.columns[i].type==="number") {
+				end_colid = i;
+				start_colid = ((i-10) >= 0) ? (i-10) : 0;
+			} else {
+				this.legend_select.append('<option value="'+i+'">'+name+'</option>');
+			}
+		}
+		this.end_select.html(this.start_select.html());
+		this.start_select.val(this.conf.start_colid || start_colid);
+		this.end_select.val(this.conf.end_colid || end_colid);
+		if(this.conf.legend_colid) this.legend_select.val(this.conf.legend_colid);
+	},
+	
+	set_chart_width: function() {
+		var $chart = $("#chart");
+		$chart.attr("width", $chart.parent().width());
+	},
+	
+	render_chart: function(opts) {
+		if(!this.grid) return;
+		if(!opts) opts = {animationSteps: 20};
+		this.set_chart_data();
+		var context = document.getElementById("chart").getContext("2d");
+		this.conf.chart_type = this.chart_select.val();
+		this.chart = new Chart(context)[this.conf.chart_type](this.chart_data, opts);
+		this.render_legend();
+		this.set_grid_height();
+	},
+	
+	set_chart_data: function() {
+		var me = this;
+		this.conf.start_colid = parseFloat(me.start_select.val() || 0);
+		this.conf.end_colid = parseFloat(me.end_select.val() || 0);
+		var columns = $.map(this.columns, function(col, i) { 
+			return (i >=me.conf.start_colid  && i<=me.conf.end_colid) ? col.field : null;
+		});
+		this.conf.selected_rowids = this.grid.getSelectedRows();
+		var datasets = $.map(this.conf.selected_rowids, function(rowid) {
+			var row = [];
+			for(var i=0, l=columns.length; i<l; i++) {
+				row.push(parseFloat(me.grid_data[rowid][columns[i]] || 0));
+			}
+			var rgb = me.grid_data[rowid].rgb;
+			return {
+				fillColor: "rgba("+rgb+",0.5)",
+				strokeColor : "rgba("+rgb+",1)",
+				pointColor : "rgba("+rgb+",1)",
+				pointStrokeColor : "#fff",
+				data : row
+			};
+		});
+		
+		this.chart_data = {
+			labels : columns,
+			datasets : datasets
+		};
+	},
+	
+	render_legend: function() {
+		var me = this;
+		this.conf.legend_colid = parseFloat(this.legend_select.val() || 0);
+		var legend_field = this.columns[this.conf.legend_colid].field;
+		var $legend = $("#legend").empty();
+		$.each(this.grid.getSelectedRows(), function(i, rowid) {
+			var row = me.grid_data[rowid];
+			$legend.append('<div class="row">\
+					<div class="legend-circle" style="background-color: rgba('+row.rgb+',0.5); \
+						border: 2px solid rgb('+row.rgb+')"></div>\
+					<span>'+row[legend_field]+'</span>\
+				</div>');
+		});
+	},
+	
+	set_grid_height: function() {
+		var chart_height = $("#chart").parent().height();
+		var window_height = $(window).height() - 50;
+		if(window_height > chart_height) chart_height = window_height;
+
+		$("#slickgrid").css("height", chart_height).attr("height", chart_height);
+		this.grid.resizeCanvas();
+	}
+});
